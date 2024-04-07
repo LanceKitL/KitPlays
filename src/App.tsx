1caeec0e77c7007994f784610efbf80ea0572b4b
LanceKitL <98525004+LanceKitL@users.noreply.github.com>
2024-04-07 18:31:41 +0800

    Update Test

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import useFetch from './components/useFetch';
 
 function App() {
   const [genre, setGenre] = useState('Games');
-  const [data, isLoading] = useFetch('http://localhost:5000/api/api1/games'); // Initial Load
-  const [genreList, isLoading2] = useFetch(`http://localhost:5000/api/api1/games?category=${genre}`); // WITH SELECTED GENRE LOAD
+  const [data, isLoading] = useFetch('https://www.mmobomb.com/api1/games'); // Initial Load
+  const [genreList, isLoading2] = useFetch(`https://www.mmobomb.com/api1/games?category=${genre}`); // WITH SELECTED GENRE LOAD
 
   const handleClick = (value: any) => {
     console.log("THIS IS APP", value);
